d13cc018b250f36b5f8fdefffd6f9fbdb531ccab
Stanislas Raczynski <stan.racz7@gmail.com>
2020-03-16 13:30:05 +0100

    Ajout multiplications sur la page /tables

---
+++ src/components/LesTables.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <span v-for="n in choice" :key="n">
+      <p>
+        <table-multiplication v-bind:n="parseInt(n)"/>&nbsp;
+      </p>
+    </span>
+    </div>
+</template>
+
+<script>
+    import TableMultiplication from './TableMultiplication.vue'
+    export default {
+        name: 'LesTables',
+        components: {
+            TableMultiplication
+        },
+        props: {
+            n: Number,
+            choice: Number
+        }
+    }
+</script>
+
+<style>
+    p {
+        display: inline-block;
+        margin: 0px 20px;
+    }
+</style>
---
+++ src/views/Tables.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h3>Les tables de multiplications</h3>
+        <input type="radio" id="10" name="10" value="10" v-model="choice">
+        <label for="10">10</label>
+        <input type="radio" id="13" name="13" value="13" v-model="choice">
+        <label for="13">13</label><br>
+        <LesTables v-bind:choice="parseInt(choice)"/>
+    </div>
+</template>
+
+<script>
+    import LesTables from '../components/LesTables.vue'
+    export default {
+        name: 'tables',
+        data: function () {
+            return {
+                choice: 10
+            }
+        },
+        components: {
+            LesTables,
+        }
+    }
+</script>
